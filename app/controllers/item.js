7829a2add6745817c60d032328301792c88080af
Benjamin Mukadi <Franckysourya@gmail.com>
2021-08-27 12:38:14 +0300

    helpers part 1

--- app/controllers/item.js
+++ app/controllers/item.js
@@ -1,16 +1,16 @@
 import Controller from '@ember/controller';
-import {tracked} from '@glimmer/tracking';
-import {action} from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ItemController extends Controller {
-@tracked color = 'red';
+  @tracked color = this.model.colors[0].color;
 
-get productImage(){
-	return `/assets/images/beats-solo-${this.color}.png`
-}
+  get productImage() {
+    return this.model.colors.find(({ color }) => color === this.color).image;
+  }
 
-@action
-onChangeColor(newColor){
-	this.color = newColor;
-}
+  @action
+  onChangeColor(newColor) {
+    this.color = newColor;
+  }
 }
